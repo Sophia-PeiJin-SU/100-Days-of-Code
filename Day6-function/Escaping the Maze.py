3e6071e454c45acc260a632d7c055a9c8211c7d2
Sophia-PeiJin-SU <tj790929@gmail.com>
2020-11-11 21:37:35 +1030

    Day 6

--- Day6-function/Escaping the Maze.py
+++ Day6-function/Escaping the Maze.py
@@ -3,18 +3,11 @@ def turn_right():
     turn_left()
     turn_left()
 
-while not is_facing_north():
-    turn_right()
-
 while not at_goal():    
     if right_is_clear():
         turn_right()
         move()
-    elif wall_in_front() and wall_on_right():
-       turn_left()
     elif wall_in_front():
-        turn_right()
-    elif wall_on_right():
-        move()
-    else:
+       turn_left()
+    elif front_is_clear():
         move()
